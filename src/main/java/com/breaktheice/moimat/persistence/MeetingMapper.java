0b538cde5dcc019ad308fbe439cc87ab737f891c
ParkKyoungnam2 <rudska5051@gmail.com>
2019-09-11 14:57:27 +0900

    추가 : 정모참석자 목록가져오기 및 테스트
    테스트 자체는 성공
    결과값에 다른테이블의 내용이 필요할까?에 대한 고민은 해봐야함
    당장은 참여자 목록에
    사진 이름 정도만 나온다고 생각함.

---
+++ src/main/java/com/breaktheice/moimat/persistence/MeetingMapper.java
@@ -0,0 +1,12 @@
+package com.breaktheice.moimat.persistence;
+
+import java.util.List;
+
+import com.breaktheice.moimat.domain.MemberVO;
+
+public interface MeetingMapper {
+	//모임 참석자 명단
+	public List<MemberVO> getMeetingMember(Long seq);
+	//MeetVO의 Long seq;
+
+}
