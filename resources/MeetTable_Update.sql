0cb94cf4b72a4e8731be220b778208784fa344f7
wy-hash <50538640+wy-hash@users.noreply.github.com>
2019-09-11 15:49:29 +0900

    feature/meet테이블 컬럼추가 #16 (#18)
    * Feature/sql table data (#11)
    
    * 추가: #3 DB 테이블 생성 및 데이터 추가
    
    DB 테이블 생성 및 데이터 추가
    1. Create_Table.sql 실행
    2. Insert_Data.sql 실행
    
    * 추가: #10 파일첨부 테이블 추가
    
    게시물에서 파일첨부시
    1. 파일이름
    2. 게시물 번호
    3. 등록일을 저장하기 위한 테이블을 추가 합니다.
    
    * 추가: feature/meet테이블 컬럼추가 #16
    
    feature/meet테이블 컬럼추가 #16
    컬럼 title, content, payment가 추가되었습니다.
    추가된 부분 쿼리 실행 부탁드립니다.

--- resources/MeetTable_Update.sql
+++ resources/MeetTable_Update.sql
@@ -11,4 +11,32 @@
 update meet 
 set member_seq = 1
 where member_seq is null;
+-- 컬럼 not null조건 추가
 Alter table meet modify (member_seq constraint NN_meet_member_seq NOT NULL);
+
+-------------- 추가 컬럼 title, content, payment
+
+--   title         VARCHAR2(50)     NOT NULL,  -- 정모이름
+ ALTER TABLE meet add(title VARCHAR2(50)) ;
+--   content       VARCHAR2(400)    NOT NULL, -- 정모 내용
+ ALTER TABLE meet add(content VARCHAR2(400)) ;
+--  payment       VARCHAR2(50)     NOT NULL, -- 회비
+ ALTER TABLE meet add(payment VARCHAR2(50)) ;
+
+-- 정모 테이블 null 값 공백으로 대체
+update meet 
+set title = ' '
+where title is null;
+
+update meet 
+set content = ' '
+where content is null;
+
+update meet 
+set payment = ' '
+where payment is null;
+
+-- 컬럼 not null조건 추가
+Alter table meet modify (title constraint NN_meet_title NOT NULL);
+Alter table meet modify (content constraint NN_meet_content NOT NULL);
+Alter table meet modify (payment constraint NN_meet_payment NOT NULL);
