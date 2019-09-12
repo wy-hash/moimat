4a4b501db62ee93fe266504a7d173b4c970263d1
ParkKyoungnam2 <rudska5051@gmail.com>
2019-09-12 18:21:20 +0900

    추가 :  MeetingService,MeetingServiceImpl

---
+++ src/main/java/com/breaktheice/moimat/service/MeetingService.java
@@ -0,0 +1,5 @@
+package com.breaktheice.moimat.service;
+
+public interface MeetingService {
+
+}
---
+++ src/main/java/com/breaktheice/moimat/service/MeetingServiceImpl.java
@@ -0,0 +1,8 @@
+package com.breaktheice.moimat.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MeetingServiceImpl implements MeetingService{
+
+}
