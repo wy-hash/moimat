5d72456b8ee7d68b3552ae009991039e181c8634
wy-hash <wy.hash@gmail.com>
2019-09-17 12:03:14 +0900

    추가: Mapper.xml 및 테스트 케이스 작성
    post - Mapper.xml 작성
    Junit 테스트 작성

--- src/main/java/com/breaktheice/moimat/persistence/PostMapper.java
+++ src/main/java/com/breaktheice/moimat/persistence/PostMapper.java
@@ -2,13 +2,24 @@ package com.breaktheice.moimat.persistence;
 
 import java.util.List;
 
+import com.breaktheice.moimat.domain.MemberVO;
 import com.breaktheice.moimat.domain.PostDomain;
 
 public interface PostMapper {
-
+	
+	// 조회수 증가
+	public int viewCount(PostDomain domain);
+	// 게시물 목록
 	public List<PostDomain> list(PostDomain domain);
+	// 게시물 상세
 	public PostDomain view(PostDomain domain);
+	// 게시물 추가
 	public int add(PostDomain domain);
+	// 게시물 수정
 	public int update(PostDomain domain);
+	// 게시물 삭제
 	public int remove(PostDomain domain);
+	
+	// 게시물 작성자 확인 member_seq 를 int 형으로 반환
+	public int memberCheck(PostDomain domain);
 }
