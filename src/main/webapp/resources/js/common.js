8ff95a8bf6c893eb21a1697ecea6496629919bd0
qsmnt <me@qsmnt.com>
2019-09-23 20:00:05 +0900

    수정: 메인 네비게이션 관련 스크립트 수정

--- src/main/webapp/resources/js/common.js
+++ src/main/webapp/resources/js/common.js
@@ -2,13 +2,14 @@
  * 모든 페이지에 필요한 공통 스크립트.
  */
 
-// 데스크탑 크기로 리사이즈시 메인네비 복원
 $(document).ready(function() {
 	$(window).resize(function() {
+		// 데스크탑 크기로 리사이즈시 메인네비 복원
 		$(window).width() >= 768 ? /mainnav/.test($('#container').attr('class')) ? $('#container').attr('class', $('#container').attr('class').replace(/(?<=mainnav\-)[^l]\w/, 'lg')) : $('#container').addClass('mainnav-lg') : '' ;
 	});
 	
 	$(window).resizeEnd(function() {
-		$(window).width() >= 768 && !(/mainnav/.test($('#container').attr('class'))) ? $('#container').addClass('mainnav-lg') : '' ;
+		// 창 키웠을때 mainnav- 관련 클래스 없으면 넣어줌.
+		if ($(window).width() >= 768 && !(/mainnav/.test($('#container').attr('class')))) $('#container').addClass('mainnav-lg');
 	});
 });
