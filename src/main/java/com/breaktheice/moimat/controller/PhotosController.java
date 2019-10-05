a9706e86c7b44178b59cd74b4030f8780d7b12bd
qsmnt <me@qsmnt.com>
2019-10-05 22:08:30 +0900

    추가: 사진게시물 등록 페이지 마크업 & 컨트롤러 작성

---
+++ src/main/java/com/breaktheice/moimat/controller/PhotosController.java
@@ -0,0 +1,18 @@
+package com.breaktheice.moimat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/groups/{groupId}/photos")
+public class PhotosController {
+
+	@GetMapping("/new")
+	public String createPost(@PathVariable("groupId") Long groupId, Model model) {
+		
+		return "/groups/photos/edit";
+	}
+}
