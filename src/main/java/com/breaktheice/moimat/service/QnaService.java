47600573a07d6f68064ab45da43a239fba36a75c
wy-hash <wy.hash@gmail.com>
2019-10-18 19:48:59 +0900

    수정: qna Service 인터페이스
    // 게시글 상세, 추가, 조회수 증가에대한 추상 메서드 등록

--- src/main/java/com/breaktheice/moimat/service/QnaService.java
+++ src/main/java/com/breaktheice/moimat/service/QnaService.java
@@ -1,6 +1,5 @@
 package com.breaktheice.moimat.service;
 
-import java.util.HashMap;
 import java.util.List;
 
 import com.breaktheice.moimat.domain.PostDomain;
@@ -12,4 +11,11 @@ public interface QnaService {
 	public List<PostDomain> myqnaList(AdminCriteria cri, Long memId);
 	// 게시글 전체 개수
 	public Long myqnaTotalCount(AdminCriteria cri, Long memId);
+	
+	// 게시글 상세
+	public PostDomain view(PostDomain domain);
+	// 게시글 추가
+	public Long add(PostDomain domain);
+	// 게시글 조회수 증가
+	public Long viewCount(PostDomain domain);
 }
